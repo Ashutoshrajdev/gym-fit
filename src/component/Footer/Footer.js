fd3d6edccf894cac308ba142ece62afe6515c11a
ashutosh <ashutosh2016raj@gmail.com>
2023-03-11 16:18:15 +0530

    initiali code

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Footer.css"
+import Github from "../../assets/github.png"
+import Insta from "../../assets/instagram.png"
+import Linkedin from "../../assets/linkedin.png"
+import Logo from "../../assets/logo.png"
+
+
+
+const Footer = () => {
+    return (
+        <div className='Footer-container'>
+
+            <div className="blur"></div>
+            <hr />
+            <div className="footer">
+                <div className="social-links">
+                <img src={Github} alt="" />
+                <img src={Insta} alt="" />
+                <img src={Linkedin} alt="" />
+            </div>
+            <div className="logo-f">
+                <img src={Logo} alt="" />
+            </div>
+            </div>
+            <div className="blur blur-f-1"></div>
+            <div className="blur blur-f-2"></div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/component/Join/Join.jsx
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react';
+import "./Join.css"
+// import emailjs from "@emailjs/browser"
+// note her to implement email functionality  here time = 1:39:00 
+
+
+const Join = () => {
+
+    const form = useRef()
+
+
+    return (
+        <div className='Join' id='jion-us'>
+            <div className="left-j">
+                <hr />
+                <div>
+                    <span className='stroke-text'>READY TO</span>
+                    <span>LEVEL UP</span>
+                </div>
+                <div>
+                    <span>YOUR BODY</span>
+                    <span className='stroke-text'>WITH US?</span>
+                </div>
+            </div>
+            <div className="right-j">
+                <form ref={form} className="email-container">
+                    <input type="email" name='user_email' placeholder='Enter your email  ' />
+                    <button className='btn btn-j'>Join Now</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Join
